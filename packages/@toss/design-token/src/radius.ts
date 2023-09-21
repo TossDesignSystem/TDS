4ebf936a7247ef5436106d5910697b5e13756970
SEOKKAMONI <sj01048260279@gmail.com>
2023-09-22 08:16:31 +0900

    feat(@toss/design-token): radius

---
+++ packages/@toss/design-token/src/radius.ts
@@ -0,0 +1,9 @@
+export type Radius = typeof radius;
+
+export const radius = {
+  radiusSmall: "4px",
+  radiusMedium: "8px",
+  radiusLarge: "16px",
+  radiusXLarge: "20px",
+  radiusXXLarge: "24px",
+} as const;
